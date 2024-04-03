44c9e57c978439fc9dba7985f19253de5155e383
Akhilpillamgolla <84654724+Akhilpillamgolla@users.noreply.github.com>
2024-04-03 11:43:24 +0530

    Add files via upload

---
+++ SecurityManual/src/main/java/com/example/demo/SecurityManualApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityManualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityManualApplication.class, args);
+	}
+
+}
